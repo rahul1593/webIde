5f03e705ac1fdc8251156a1d273b47d26825843a
Rahul Bhartari <engineerbhartarisingh@gmail.com>
2017-01-08 19:42:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel='stylesheet' type='text/css' href="css/ide.css">
+        <script type='text/javascript' src="js/jquery-1.12.4.js"></script>
+        <script type="text/javascript" src="js/micro.js"></script>
+        <title>Web IDE</title>
+    </head>
+    <body>
+        <script type="text/javascript" src="js/app.js"></script>
+    </body>
+</html>
