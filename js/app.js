807076f555eec5d427d84865fe8e5db9ca252666
Rahul Bhartari <engineerbhartarisingh@gmail.com>
2017-01-16 00:16:07 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* 
- * Author: Rahul Bhartari.
- * 
- * 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 var objExp_style = 'position:absolute;left:0%;width:18.1%;top:0%;height:99.8%;border-top:1px solid black;border-bottom:1px solid black;border-left:1px solid black';
@@ -86,14 +86,82 @@ function init_menuBar(){
     var win = u$.System.Library.Gui.createMenuBarItem(menuBarWin, 'Window', null);
     var tools = u$.System.Library.Gui.createMenuBarItem(menuBarWin, 'Tools', null);
     var help = u$.System.Library.Gui.createMenuBarItem(menuBarWin, 'Help', null);
-    //add options to file: new, open, close, save, save as, save all, download, import project, export project, exit
-    u$.System.Library.Gui.createMenuBarItemOption(file, 'New', null);
-    //add options to edit: undo, redo, cut, copy, paste, delete, find, replace
-    //add options to view: footer menu: show status, show info, toolbar menu: fileoperations, run config, edit
-    //add options to project: new project, open project, close project, options, run configuration
-    //add options to window: object explorer, editor, output, console, details, layout menu: sql, python, web, doc, reset layout
-    //add options to tools: templates, options, external menu: ..if any
-    //add options to help: Help, Documents, Report Issues, About
+    //add options to file: new, open, close, save, save as, save all, download, import project, export project, exit -----------------------------------
+    var file_cm = u$.System.Library.Gui.addContextMenuToItem(file);
+    var file_scm1_new = u$.System.Library.Gui.createContextMenuOption(file_cm, 'New File', null);
+    var file_scm1_open = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Open File', null);
+    var file_scm1_close = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Close File', null);
+    u$.System.Library.Gui.addOptionSeparator(file_cm);
+    var file_scm2_save = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Save', null);
+    var file_scm2_saveas = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Save As', null);
+    var file_scm2_saveall = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Save All', null);
+    u$.System.Library.Gui.addOptionSeparator(file_cm);
+    var file_scm3_dwnld = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Download', null);
+    var file_scm3_imp = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Import Project', null);
+    var file_scm3_exp = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Export Project', null);
+    u$.System.Library.Gui.addOptionSeparator(file_cm);
+    var file_scm4_exit = u$.System.Library.Gui.createContextMenuOption(file_cm, 'Exit', null);
+    
+    //add options to edit: undo, redo, cut, copy, paste, delete, find, replace -------------------------------------------------------------------------
+    var edit_cm = u$.System.Library.Gui.addContextMenuToItem(edit);
+    var edit_scm1_undo = u$.System.Library.Gui.createContextMenuOption(edit_cm, 'Undo', null);
+    var edit_scm1_redo = u$.System.Library.Gui.createContextMenuOption(edit_cm, 'Redo', null);
+    u$.System.Library.Gui.addOptionSeparator(edit_cm);
+    var edit_scm2_cut = u$.System.Library.Gui.createContextMenuOption(edit_cm, 'Cut', null);
+    var edit_scm2_copy = u$.System.Library.Gui.createContextMenuOption(edit_cm, 'Copy', null);
+    var edit_scm2_paste = u$.System.Library.Gui.createContextMenuOption(edit_cm, 'Paste', null);
+    var edit_scm2_delete = u$.System.Library.Gui.createContextMenuOption(edit_cm, 'Delete', null);
+    u$.System.Library.Gui.addOptionSeparator(edit_cm);
+    var edit_scm3_find = u$.System.Library.Gui.createContextMenuOption(edit_cm, 'Find', null);
+    var edit_scm3_replace = u$.System.Library.Gui.createContextMenuOption(edit_cm, 'Replace', null);
+    
+    //add options to view: footer menu: show status, show info, toolbar menu: fileoperations, run config, edit -----------------------------------------
+    var view_cm = u$.System.Library.Gui.addContextMenuToItem(view);
+    view_cm.setWidth(150);
+    var view_scm1_ssts = u$.System.Library.Gui.createContextMenuOption(view_cm, 'Show IDE Status', null);
+    var view_scm1_sinf = u$.System.Library.Gui.createContextMenuOption(view_cm, 'Show Info', null);
+    u$.System.Library.Gui.addOptionSeparator(view_cm);
+    var view_scm2_run = u$.System.Library.Gui.createContextMenuOption(view_cm, 'Show Run Toolbar', null);
+    var view_scm2_edit = u$.System.Library.Gui.createContextMenuOption(view_cm, 'Show Edit Toolbar', null);
+    
+    //add options to project: new project, open project, close project, options, run project, run configuration ----------------------------------------
+    var proj_cm = u$.System.Library.Gui.addContextMenuToItem(project);
+    //var proj_scm1 = u$.System.Library.Gui.addSubContextMenu(proj_cm);
+    var proj_scm1_new = u$.System.Library.Gui.createContextMenuOption(proj_cm, 'New Project', null);
+    var proj_scm1_open = u$.System.Library.Gui.createContextMenuOption(proj_cm, 'Open Project', null);
+    var proj_scm1_close = u$.System.Library.Gui.createContextMenuOption(proj_cm, 'Close Project', null);
+    u$.System.Library.Gui.addOptionSeparator(proj_cm);
+    var proj_scm2_run = u$.System.Library.Gui.createContextMenuOption(proj_cm, 'Run', null);
+    var proj_scm2_runc = u$.System.Library.Gui.createContextMenuOption(proj_cm, 'Run Config', null);
+    var proj_scm2_opt = u$.System.Library.Gui.createContextMenuOption(proj_cm, 'Options', null);
+    
+    //add options to window: object explorer, editor, output, console, details, layout menu: sql, python, web, doc, reset layout -----------------------
+    var win_cm = u$.System.Library.Gui.addContextMenuToItem(win);
+    win_cm.setWidth(150);
+    var win_scm1_obex = u$.System.Library.Gui.createContextMenuOption(win_cm, 'Object Explorer', null);
+    var win_scm1_editor = u$.System.Library.Gui.createContextMenuOption(win_cm, 'Editor', null);
+    var win_scm1_output = u$.System.Library.Gui.createContextMenuOption(win_cm, 'Output', null);
+    var win_scm1_console = u$.System.Library.Gui.createContextMenuOption(win_cm, 'Console', null);
+    var win_scm1_details = u$.System.Library.Gui.createContextMenuOption(win_cm, 'Details', null);
+    u$.System.Library.Gui.addOptionSeparator(win_cm);
+    var win_scm2_layout = u$.System.Library.Gui.createContextMenuOption(win_cm, 'Layout', null);
+        //add context to layout option
+    var win_scm2_lt_cm = u$.System.Library.Gui.addContextMenuToOption(win_scm2_layout);
+    var win_scm2_lt_scm1_none = u$.System.Library.Gui.createContextMenuOption(win_scm2_lt_cm, 'None', null);
+    
+    //add options to tools: templates, options, external menu: ..if any --------------------------------------------------------------------------------
+    var tools_cm = u$.System.Library.Gui.addContextMenuToItem(tools);
+    var tools_scm1_tmpl = u$.System.Library.Gui.createContextMenuOption(tools_cm, 'Templates', null);
+    var tools_scm1_prf = u$.System.Library.Gui.createContextMenuOption(tools_cm, 'preferences', null);
+    var tools_scm1_ext = u$.System.Library.Gui.createContextMenuOption(tools_cm, 'External Tools', null);
+    
+    //add options to help: Help, Documents, Report Issues, About ---------------------------------------------------------------------------------------
+    var help_cm = u$.System.Library.Gui.addContextMenuToItem(help);
+    var help_scm1_help = u$.System.Library.Gui.createContextMenuOption(help_cm, 'Help Contents', null);
+    var help_scm1_issues = u$.System.Library.Gui.createContextMenuOption(help_cm, 'Report Issues', null);
+    u$.System.Library.Gui.addOptionSeparator(help_cm);
+    var help_scm2_about = u$.System.Library.Gui.createContextMenuOption(help_cm, 'About IDE', null);
+    
 }
 
 u$.System.sysInit();
